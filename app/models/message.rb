6beb8b7e4c7f213be0252d882d00252ca182f0d1
verissimohenry <verissimohenry04@gmail.com>
2021-06-10 20:53:46 +0100

    fix linter errors

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-    belongs_to :conversation
+  belongs_to :conversation
   belongs_to :send_user, class_name: 'User'
   belongs_to :receive_user, class_name: 'User'
   validates :text, presence: true
