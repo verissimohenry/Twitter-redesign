c748a27daf31f6072bdfd795e8daa488b850f17a
verissimohenry <verissimohenry04@gmail.com>
2021-06-11 13:30:17 +0100

    add test in rspec

--- spec/models/following_spec.rb
+++ spec/models/following_spec.rb
@@ -3,18 +3,12 @@ require 'rails_helper'
 RSpec.describe Following, type: :model do
   context ' test following model' do
     before do
-      @user1 = User.new(email: 'verissimohenry04@gmail.com', fullname: 'henry', password: '123456',
-                        profile_photo: 'donat.jpeg', cover_image: 'eric.jpeg').save
-      @user2 = User.new(email: 'elvis@gmail.com', fullname: 'elvis', password: '123456',
-                        profile_photo: 'https://hajiri.co/uploads/no_image.jpg', cover_image: 'https://i.stack.imgur.com/y9DpT.jpg').save
+      @user1 = User.new(email: 'verissimohenry04@gmail.com', username: 'henry', password: '123456').save
+      @user2 = User.new(email: 'elvis@gmail.com', username: 'elvis', password: '123456').save
     end
     it 'return false if followed_id is not provided' do
       @invite = Following.create
       expect(@invite).to_not be_valid
-    end
-
-    describe 'validations' do
-      it { should validate_presence_of(:followed_id) }
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validation tests' do
     it 'returns false if not all params are filled in.' do
-      user = User.new(email: 'verissimohenry04@gmail.com', fullname: 'HENRY').save
+      user = User.new(email: 'verissimohenry04@gmail.com', username: 'HENRY').save
       expect(user).to eq(false)
     end
 
     it 'returns false if email is not valid.' do
-      user = User.new(email: 'verissimohenry04@gmail.com', fullname: 'HENRY').save
+      user = User.new(email: 'verissimohenry04@gmail.com', username: 'HENRY').save
       expect(user).to eq(false)
     end
 
@@ -16,20 +16,14 @@ RSpec.describe User, type: :model do
       expect(User.find_by(email: 'elvis')).to eq(nil)
     end
 
-    it 'returns false if udername is not not provided' do
-      user = User.new(email: 'verissimohenry04@gmail.com', password: '123456',
-                      profile_photo: 'https://hajiri.co/uploads/no_image.jpg', cover_image: 'https://i.stack.imgur.com/y9DpT.jpg').save
+    it 'returns false if udername is not provided' do
+      user = User.new(email: 'verissimohenry04@gmail.com', password: '123456').save
       expect(user).to eq(false)
     end
 
     it 'returns false if password values are less than 6 ' do
-      user = User.new(email: 'verissimohenry04@gmail.com', fullname: 'HENRY', password: '123456', profile_photo: 'https://hajiri.co/uploads/no_image.jpg',
-                      cover_image: 'https://i.stack.imgur.com/y9DpT.jpg').save
+      user = User.new(email: 'verissimohenry04@gmail.com', username: 'HENRY', password: '12345').save
       expect(user).to eq(false)
-    end
-
-    describe 'validations' do
-      it { should validate_presence_of(:fullname) }
     end
   end
 end
