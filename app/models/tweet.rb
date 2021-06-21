d0e0e7ba9e8cf8a2fea0931e03547e2a7f95cb3d
verissimohenry <verissimohenry04@gmail.com>
2021-06-21 16:54:27 +0100

    fix linter error

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   validates :text, presence: true
-  scope :withlike, -> { includes(:likes)}
-  scope :withauthor, -> { includes(:author)}
+  scope :withlike, -> { includes(:likes) }
+  scope :withauthor, -> { includes(:author) }
   # Ex:- scope :active, -> {where(:active => true)}
 end
