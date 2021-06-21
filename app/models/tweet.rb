212de9fe09182bde61936989fdfcba641fcae19a
verissimohenry <verissimohenry04@gmail.com>
2021-06-21 16:05:39 +0100

    modify tweet folder

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,5 +3,6 @@ class Tweet < ApplicationRecord
   has_many :likes
   validates :text, presence: true
   scope :withlike, -> { includes(:likes)}
+  scope :withauthor, -> { includes(:author)}
   # Ex:- scope :active, -> {where(:active => true)}
 end
